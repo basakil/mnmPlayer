17134be840cd49eb4ef8c0e08561fbd8e7230a02
basakil <basakil@noreply.com>
2016-07-14 07:36:06 +0300

    Initial Commit

---
+++ mnmPlayer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  mnmPlayer
+//
+//  Created by aobskl on 7/14/16.
+//  Copyright © 2016 AoB. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
